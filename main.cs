a0beadf3426eb7f59997d8268b78ddc002a9f2b1
Aulobox <>
2022-12-02 00:00:36 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //declara variavel
+    //var nome : caracter
+   string nome;
+    
+    //leia (nome)
+    nome = Console.ReadLine();
+
+    //Escreval("Hello World")
+    Console.WriteLine("Hello"+ nome);
+  }
+}
